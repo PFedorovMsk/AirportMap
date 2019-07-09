8e7ea809da266ce1699f39d6ee79dff1d8467484
Pavel Fedorov <pavel.fedorov.msk@gmail.com>
2019-07-09 06:55:37 +0300

    Подключил стандартные переводы (почему-то системные диалоги были на-английском при русской локали в системе -_-)

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include "main_window.h"
 #include <QApplication>
+#include <QLibraryInfo>
+#include <QTranslator>
 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow   window;
+
+    QString     locale = QLocale::system().name();
+    QTranslator qtTranslator;
+    if (qtTranslator.load("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath)))
+        app.installTranslator(&qtTranslator);
+
+    MainWindow window;
     window.show();
 
     return app.exec();
